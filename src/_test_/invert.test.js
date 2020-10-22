323696128acfe73759f4d496588db9f03de24d34
Lom125 <67274378+Lom125@users.noreply.github.com>
2020-10-22 17:17:57 +0300

    Add files via upload

---
+++ src/_test_/invert.test.js
@@ -0,0 +1,5 @@
+import { invertString } from "../invert.js";
+
+describe("tests for invertString function", () => {
+    it("should reverse string", () => expect(invertString("Enter X")).toBe("X retnE"));
+  });
---
+++ src/invert.js
@@ -0,0 +1,6 @@
+export function invertString(str) {
+    let invertStr
+    invertStr = str.split("").reverse().join("");
+    return invertStr;
+   }
+
